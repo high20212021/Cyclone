081e91a928646dee70ae486e969f8c1984057af9
Sunch233 <2125696621@qq.com>
2024-06-29 18:53:18 +0800

    Update Utils.php

--- src/pocketmine/utils/Utils.php
+++ src/pocketmine/utils/Utils.php
@@ -429,4 +429,20 @@ class Utils{
 		return $hash;
 	}
 
+	/**
+	 * Generator which forces array keys to string during iteration.
+	 * This is necessary because PHP has an anti-feature where it casts numeric string keys to integers, leading to
+	 * various crashes.
+	 *
+	 * @phpstan-template TKeyType of string
+	 * @phpstan-template TValueType
+	 * @phpstan-param array<TKeyType, TValueType> $array
+	 * @phpstan-return \Generator<TKeyType, TValueType, void, void>
+	 */
+	public static function stringifyKeys(array $array): \Generator{
+		foreach($array as $key => $value){
+			yield (string)$key => $value;
+		}
+	}
+
 }
