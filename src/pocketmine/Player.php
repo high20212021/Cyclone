8f1326e6932494748d0fda616a594f8e0d5de92d
Sunch233 <2125696621@qq.com>
2024-06-30 16:11:45 +0800

    call PlayerInteractEvent when ItemFrame drop

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -3404,9 +3404,15 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 
 				$tile = $this->level->getTile($this->temporalVector->setComponents($packet->x, $packet->y, $packet->z));
 				if($tile instanceof ItemFrame){
+					$ev = new PlayerInteractEvent($this, $this->inventory->getItemInHand(), $tile->getBlock(), 5 - $tile->getBlock()->getDamage(), PlayerInteractEvent::LEFT_CLICK_BLOCK);
+					$this->server->getPluginManager()->callEvent($ev);
 					if($this->isSpectator()){
+						$ev->setCancelled();
+					}
+
+					if($ev->isCancelled()){
 						$tile->spawnTo($this);
-						break;
+						return true;
 					}
 
 					if(lcg_value() <= $tile->getItemDropChance()){
