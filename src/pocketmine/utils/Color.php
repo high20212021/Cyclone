2b04f4f3cc55c9c1f9f33207349a71302118df3f
Sunch233 <2125696621@qq.com>
2024-06-30 10:41:08 +0800

    color: compatible with PMMP

--- src/pocketmine/utils/Color.php
+++ src/pocketmine/utils/Color.php
@@ -44,9 +44,8 @@ class Color {
 	const COLOR_DYE_ORANGE = 14;
 	const COLOR_DYE_WHITE = 15;
 
-	private $red = 0;
-	private $green = 0;
-	private $blue = 0;
+	/** @var int */
+	protected $a, $r, $g, $b;
 
 	/** @var \SplFixedArray */
 	public static $dyeColors = null;
@@ -98,29 +97,155 @@ class Color {
 		return Color::getRGB(0, 0, 0);
 	}
 
-	public function __construct($r, $g, $b){
-		$this->red = $r;
-		$this->green = $g;
-		$this->blue = $b;
+	public function __construct(int $r, int $g, int $b, int $a = 0xff){
+		$this->r = $r & 0xff;
+		$this->g = $g & 0xff;
+		$this->b = $b & 0xff;
+		$this->a = $a & 0xff;
 	}
 
 	public function getRed(){
-		return (int) $this->red;
+		return $this->r;
 	}
 
 	public function getBlue(){
-		return (int) $this->blue;
+		return $this->g;
 	}
 
 	public function getGreen(){
-		return (int) $this->green;
+		return $this->b;
 	}
 
 	public function getColorCode(){
-		return ($this->red << 16 | $this->green << 8 | $this->blue) & 0xffffff;
+		return ($this->r << 16 | $this->g << 8 | $this->b) & 0xffffff;
 	}
 
 	public function __toString(){
-		return "Color(red:" . $this->red . ", green:" . $this->green . ", blue:" . $this->blue . ")";
+		return "Color(red:" . $this->r . ", green:" . $this->g . ", blue:" . $this->b . ")";
+	}
+
+	// Genisys
+	//=============
+	// PMMP
+
+	/**
+	 * Returns the alpha (transparency) value of this colour.
+	 * @return int
+	 */
+	public function getA() : int{
+		return $this->a;
+	}
+
+	/**
+	 * Sets the alpha (opacity) value of this colour, lower = more transparent
+	 * @param int $a
+	 */
+	public function setA(int $a){
+		$this->a = $a & 0xff;
+	}
+
+	/**
+	 * Retuns the red value of this colour.
+	 * @return int
+	 */
+	public function getR() : int{
+		return $this->r;
+	}
+
+	/**
+	 * Sets the red value of this colour.
+	 * @param int $r
+	 */
+	public function setR(int $r){
+		$this->r = $r & 0xff;
+	}
+
+	/**
+	 * Returns the green value of this colour.
+	 * @return int
+	 */
+	public function getG() : int{
+		return $this->g;
+	}
+
+	/**
+	 * Sets the green value of this colour.
+	 * @param int $g
+	 */
+	public function setG(int $g){
+		$this->g = $g & 0xff;
+	}
+
+	/**
+	 * Returns the blue value of this colour.
+	 * @return int
+	 */
+	public function getB() : int{
+		return $this->b;
+	}
+
+	/**
+	 * Sets the blue value of this colour.
+	 * @param int $b
+	 */
+	public function setB(int $b){
+		$this->b = $b & 0xff;
+	}
+
+	/**
+	 * Returns a Color from the supplied RGB colour code (24-bit)
+	 * @param int $code
+	 *
+	 * @return Color
+	 */
+	public static function fromRGB(int $code){
+		return new Color(($code >> 16) & 0xff, ($code >> 8) & 0xff, $code & 0xff);
+	}
+
+	/**
+	 * Returns a Color from the supplied ARGB colour code (32-bit)
+	 *
+	 * @param int $code
+	 *
+	 * @return Color
+	 */
+	public static function fromARGB(int $code){
+		return new Color(($code >> 16) & 0xff, ($code >> 8) & 0xff, $code & 0xff, ($code >> 24) & 0xff);
+	}
+
+	/**
+	 * Returns an ARGB 32-bit colour value.
+	 * @return int
+	 */
+	public function toARGB() : int{
+		return ($this->a << 24) | ($this->r << 16) | ($this->g << 8) | $this->b;
+	}
+
+	/**
+	 * Returns a little-endian ARGB 32-bit colour value.
+	 * @return int
+	 */
+	public function toBGRA() : int{
+		return ($this->b << 24) | ($this->g << 16) | ($this->r << 8) | $this->a;
+	}
+
+	/**
+	 * Returns an RGBA 32-bit colour value.
+	 * @return int
+	 */
+	public function toRGBA() : int{
+		return ($this->r << 24) | ($this->g << 16) | ($this->b << 8) | $this->a;
+	}
+
+	/**
+	 * Returns a little-endian RGBA colour value.
+	 * @return int
+	 */
+	public function toABGR() : int{
+		return ($this->a << 24) | ($this->b << 16) | ($this->g << 8) | $this->r;
+	}
+
+	public static function fromABGR(int $code){
+		return new Color($code & 0xff, ($code >> 8) & 0xff, ($code >> 16) & 0xff, ($code >> 24) & 0xff);
 	}
 }
